e0535dbe8faa9078cf1056720b1c6f1873a87b4f
Joana Araújo <120012238+jaraujo09@users.noreply.github.com>
2023-09-25 11:48:18 +0100

    changes

--- datasets/iris/eval_1.py
+++ datasets/iris/eval_1.py
@@ -1,30 +1,31 @@
 import sys
 import numpy as np
 import pandas as pd
-from src.si.data.dataset import Dataset
-from src.si.io.csv_file import read_csv, write_csv 
-from src.si.io.data_file import *
+from si.data.dataset import Dataset
+from si.io.csv_file import read_csv, write_csv 
+from si.io.data_file import *
 
 filename = 'C:\\Users\\Fofinha\\Desktop\\UNI\\MESTRADO\\2o ANO\\Sistemas Inteligentes\\si\\datasets\\iris\\iris.csv'
 
 #1.1.
 dataset = read_csv(filename, sep = ',', features = True, label = True)
-dataset.shape()
+print(dataset.shape())
 
 #1.2.
 penultimate = dataset.X[:, -2]
-penultimate.shape()
+print(penultimate.shape)
 
 #1.3.
 last_10 = dataset.X[-10:]
-last_10.get_mean()
+print(last_10.mean())
+
 
 #1.4.
 selected_samples = len(dataset.X[dataset.X <= 6])
-
+print(selected_samples)
 
 #1.5.
-not_setosa = len(dataset.X[dataset.y['class'] != 'Iris-setosa'])
-
+not_setosa = len(dataset.X[dataset.y!= 'Iris-setosa'])
+print(not_setosa)
 
 #2.
