8b8df758ddc764d2be2da66cc46130714ca0b61e
Joana Araújo <120012238+jaraujo09@users.noreply.github.com>
2023-10-03 16:18:31 +0100

    pca class done

--- src/si/decomposition/pca.py
+++ src/si/decomposition/pca.py
@@ -2,71 +2,93 @@ import numpy as np
 from si.data.dataset import Dataset
 
 class PCA:
-    def __init__(self, n_components: int):
+    """
+    PCA implementation to reduce the dimensionality of a given dataset. It uses SVD (Singular
+    Value Decomposition) to do it.
+    """
+    def __init__(self, n_components: int = 10):
+        """
+        PCA implementation to reduce the dimensionality of a given dataset. It uses SVD (Singular
+        Value Decomposition) to do it.
+
+        Parameters
+        ----------
+        n_components: int (default=10)
+            The number of principal components to be computed
+
+        Attributes
+        ----------
+        mean: np.ndarray
+            The mean value of each feature of the dataset
+        components: np.ndarray
+            The first <n_components> principal components
+        explained_variance: np.ndarray
+            The variances explained by the first <n_components> principal components
+        """
         self.n_components = n_components
 
         self.mean = None
         self.components = None
         self.explained_variance = None
 
-    #Before implementing fit method of PCA: SVD method
-
-    def _centered_data(self, dataset: Dataset) -> np.ndarray:
-        self.mean = np.mean(dataset.X, axis = 0) #mean for each column
-        
-        return dataset.X - self.mean
-    
-    def _get_principal_components(self, dataset: Dataset) -> np.ndarray:
-        centered_data = self._centered_data(dataset)
-        
-        #calculate SVD
-        self.u, self.s, self.v = np.linalg.svd(centered_data, full_matrices=False)
-
-        #components corresponde to the first n_components of V^T
-        self.components = self.v[:, :self.n_components] # get the first n_components columns
-
-        return self.components
-    
-    def _get_explained_variance(self, dataset:Dataset)-> np.ndarray:
-        ev = (self.s ** 2)/(len(dataset.X)-1)
-
-        #The explained variance (explained_variance) corresponds to the first n_components of EV
-        explained_variance = ev[:self.explained_variance]
 
     def fit(self, dataset: Dataset) -> 'PCA':
-        self.components = self._get_principal_components(dataset)
-        self.explained_variance = self._get_explained_variance(dataset)
+        """
+        Fits PCA by computing the mean value of each feature of the dataset, the first 
+        <n_components> principal components and the corresponding explained variances.
+        Returns self.
 
+        Parameters
+        ----------
+        dataset: Dataset
+            A Dataset object
+        """
+        #firstly, center data
+        self.mean = np.mean(dataset.X, axis = 0) #mean for each column
+        self.centered_data = dataset.X - self.mean
+
+        #calcule of SVD
+        u,s,vt = np.linalg.svd(self.centered_data, full_matrices=False)
+        
+        #components corresponde to the first n_components of V^T
+        self.components = vt[:self.n_components]
+
+        #The explained variance (explained_variance) corresponds to the first n_components of EV
+        ev = (self.s ** 2)/(len(dataset.X)-1)
+        explained_variance = ev[:self.n_components]
+        
         return self
     
     def transform(self, dataset:Dataset)-> Dataset:
+        """
+        Transforms the dataset by reducing X (X_reduced = X @ V, V = self.components.T).
+        Returns X reduced.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            A Dataset object
+        """
         if self.components is None:
             raise Exception ("You should 'fit' PCA before calling 'transform")
         centered_data = self._centered_data(dataset)
+
+        v_matrix = self.components.T
+
+        # Get transformed data
+        transformed_data = np.dot(self.centered_data, v_matrix)
         
-        return np.dot(centered_data, self.components.T)
+        return Dataset(transformed_data, dataset.y, dataset.features_names, dataset.label_name)
 
     def fit_transform(self, dataset:Dataset)-> np.ndarray:
+        """
+        Fits PCA and transforms the dataset by reducing X. Returns X reduced.
+        
+        Parameters
+        ----------
+        dataset: Dataset
+            A Dataset object
+        """
         self.fit(dataset)
         return self.transform(dataset)
     
-if __name__ == 'main':
-    from si.io.csv_file import read_csv_file
-
-    print("EX1")
-    ds = Dataset.from_random(n_examples=10, n_features=10, label=False, seed=0)
-    pca = PCA(n_components=4)
-    pca.fit(ds)
-    print(pca.mean)
-    print(pca._get_principal_components)
-    print(pca._get_explained_variance)
-    x_reduced = pca.transform(ds)
-    print(x_reduced)
-    
-    print("\nEX2 - iris")
-    path = "../../../datasets/iris/iris.csv"
-    iris = read_csv_file(file=path, sep=",", features=True, label=True)
-    pca_iris = PCA(n_components=2)
-    iris_reduced = pca_iris.fit_transform(iris)
-    print(iris_reduced)
-
