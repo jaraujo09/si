cf8e764d747720d63a199b3c16723ca17511b527
Joana Araújo <120012238+jaraujo09@users.noreply.github.com>
2023-10-17 10:21:14 +0100

    categoricalNB try

--- src/si/models/categorical_nb.py
+++ src/si/models/categorical_nb.py
@@ -4,7 +4,7 @@ import numpy as np
 class CategoricalNB:
     def __init__(self, smoothing: float = 1.0):
         """
-        Initialize the Categorical Naive Bayes
+        Initialize the Categorical Naive Bayes (suitable for categorical/discrete data)
 
         Parameters
         ----------
@@ -13,3 +13,31 @@ class CategoricalNB:
         distance: Callable
             The distance function to use - euclidean distance
         """
+        #parameter
+        self.smoothing = smoothing
+
+        #atributes
+        self.class_prior = None
+        self.feature_prob = None
+
+    def fit(self, dataset:Dataset)-> 'CategoricalNB':
+        """
+        It fits the model to the given dataset
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to fit the model to
+
+        Returns
+        -------
+        self: CategoricalNB
+            The fitted model
+        """
+        n_samples = dataset.shape[0]
+        n_features = dataset.features
+        n_classes = dataset.y
+
+        class_counts = len(n_classes)
+        feature_counts = len(n_classes , n_features)
+        class_prior = len(n_classes)
