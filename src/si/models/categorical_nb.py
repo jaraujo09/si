3cdf1238a123f214387bc898d1900a10f45c05f2
Joana Araújo <120012238+jaraujo09@users.noreply.github.com>
2023-10-17 12:11:12 +0100

    nb + knn_regressor

--- src/si/models/categorical_nb.py
+++ src/si/models/categorical_nb.py
@@ -1,4 +1,5 @@
 from si.data.dataset import Dataset
+from si.metrics.accuracy import accuracy
 import numpy as np
 
 class CategoricalNB:
@@ -10,19 +11,19 @@ class CategoricalNB:
         ----------
         smoothing: float
             Laplace smoothing to avoid zero probabilities
-        distance: Callable
-            The distance function to use - euclidean distance
         """
-        #parameter
-        self.smoothing = smoothing
+        # Parameters
+        self.smoothing = smoothing  # Parameter to avoid probabilities of 0
 
-        #atributes
+        # Attributes
         self.class_prior = None
         self.feature_prob = None
+        self.n_classes = 0
 
-    def fit(self, dataset:Dataset)-> 'CategoricalNB':
+    def fit(self, dataset: Dataset) -> 'CategoricalNB':
         """
-        It fits the model to the given dataset
+        Fit the model to the given dataset.
+        Estimate class_prior and feature_probs from the Dataset.
 
         Parameters
         ----------
@@ -34,10 +35,91 @@ class CategoricalNB:
         self: CategoricalNB
             The fitted model
         """
-        n_samples = dataset.shape[0]
-        n_features = dataset.features
-        n_classes = dataset.y
+        n_samples, n_features = dataset.shape()
+        labels = dataset.get_classes()
+        n_classes = len(np.unique(labels))
+        self.n_classes = n_classes
+        self.n_features = n_features 
 
-        class_counts = len(n_classes)
-        feature_counts = len(n_classes , n_features)
-        class_prior = len(n_classes)
+        class_counts = np.zeros(n_classes)
+        feature_counts = np.zeros((n_classes, n_features))  # Ensure correct shape
+        class_prior = np.zeros(n_classes)
+
+        for i in range(n_samples):
+            sample = dataset.X[i]
+            class_label = int(sample[-1])  # Class label is in the last column
+            class_counts[class_label] += 1  # Counting how many times that class_label appears in the training set
+            feature_counts[class_label, :] += sample # Counting the frequency of each value for each class
+
+        class_counts += (n_features * self.smoothing)
+        feature_counts += self.smoothing  # Laplace smoothing, avoiding probabilities of 0
+
+        self.class_prior = class_counts / n_samples
+        self.feature_prob = feature_counts / class_counts[:, np.newaxis]
+
+        return self
+
+    def predict(self, dataset: Dataset) -> np.ndarray:
+        """
+        Predict the class labels for a given set of samples.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to predict the classes of
+
+        Returns
+        -------
+        predictions: np.ndarray
+            The predictions of the model
+        """
+        class_probs = np.zeros(self.n_classes)  # Initialize an array to store class probabilities
+        predictions = []
+
+        for sample in dataset.X:
+            for c in range(self.n_classes):
+                prob = np.prod(sample * self.feature_prob[c] + (1 - sample) * (1 - self.feature_prob[c])) * self.class_prior[c]
+                class_probs[c] = prob
+
+            predicted_class = np.argmax(class_probs)
+            predictions.append(predicted_class)
+
+        return np.array(predictions)
+
+    def score(self, dataset: Dataset) -> float:
+        """
+        Calculate the accuracy between actual values and predictions.
+
+        Parameters
+        ----------
+        dataset: Dataset
+            The dataset to evaluate the model on
+
+        Returns
+        -------
+        error: float
+            Error between actual values and predictions
+        """
+        predictions = self.predict(dataset)
+        return accuracy(dataset.y, predictions)
+
+if __name__ == '__main__':
+    # import dataset
+    from si.data.dataset import Dataset
+    from si.model_selection.split import train_test_split
+
+    # load and split the dataset
+    dataset_ = Dataset.from_random(600, 100, 2)
+    dataset_train, dataset_test = train_test_split(dataset_, test_size=0.2)
+
+    # initialize the KNN classifier
+    nb = CategoricalNB(smoothing=1.0)
+
+    # fit the model to the train dataset
+    nb.fit(dataset_train)
+
+    prediction = nb.predict(dataset_test)
+
+    # evaluate the model on the test dataset
+    score = nb.score(dataset_test)
+    print(f'The accuracy of the model is: {score}')
--- src/si/models/knn_regressor.py
+++ src/si/models/knn_regressor.py
@@ -1,30 +1,48 @@
-from si.data.dataset import Dataset
-from si.metrics import rmse
-from si.model_selection import split
-from si.statistics import euclidean_distance
 import numpy as np
-from typing import Callable, Union
+from si.data.dataset import Dataset
+from si.statistics.euclidean_distance import euclidean_distance
+from si.metrics.accuracy import accuracy
+from si.metrics.rmse import rmse
+from si.model_selection.split import stratified_train_test_split
+from typing import Callable,Union
+from si.model_selection.split import train_test_split
 
 class KNNRegressor:
-    def __init__(self, k: int = 1, distance: Callable = euclidean_distance):
+    """
+    The k-Nearest Neighbors regressor is a machine learning model that predicts the value of a new sample based on 
+    a similarity measure (e.g., distance functions). This algorithm estimates the value of a new sample by
+    considering the values(mean) of the k-nearest samples in the training data.
+
+    Parameters
+    ----------
+    k: int
+        The number of nearest neighbors to use
+    distance: Callable
+        The distance function to use
+
+    Attributes
+    ----------
+    dataset: np.ndarray
+        The training data
+    """
+    def __init__(self,k:int=1,distance:Callable=euclidean_distance):
         """
-        Initialize the KNN Regressor
-
-        Parameters
-        ----------
-        k: int
-            The number of nearest neighbors to use
-        distance: Callable
-            The distance function to use - euclidean distance
+        This algorithm predicts the class for a sample using the k most similar examples.But is suitable for regression problems.
+        So estimates the average value of the k most similar examples instead of the most common class.
+        Args:
+            k :int 
+                number of examples to consider
+            distance: Callable 
+                euclidean distance function. .
         """
-        # parameters
-        self.k = k
-        self.distance = distance
+        #parameters
+        self.k=k # numero de k-mais proximos exemplos a considera
+        
+        #atributes
+        self.distance=distance #distance function between each sample to the samples of the train dataset
+        self._train_dataset=None # train dataset
 
-        # attributes
-        self.train_dataset = None
-
-    def fit(self, dataset:Dataset) -> 'KNNRegressor':
+    def fit(self, dataset:Dataset) -> "KNNRegressor":
         """
         It fits the model to the given dataset
 
@@ -38,10 +56,10 @@ class KNNRegressor:
         self: KNNRegressor
             The fitted model
         """
-        self.dataset = dataset
+        self._train_dataset=dataset # o input é o dataset de treino logo apenas fiz este passo-guardar o dataset treino
         return self
     
-    def _get_closest_label(self, sample: np.ndarray) -> float:
+    def _get_closest_value_label(self,sample:np.ndarray)->int:
         """
         It returns the closest label of the given sample
 
@@ -55,17 +73,17 @@ class KNNRegressor:
         label: str or int
             The closest label
         """
-        distances = self.distance(sample, self.train_dataset.X)   #distance between each sample
-        k_similar = np.argsort(distances)[:self.k]    #indexes of the k most similar examples in crescent order
-        k_similar_values = self.train_dataset.y[k_similar]  
+        distances=self.distance(sample,self._train_dataset.X) #distance between each sample to the samples of the train dataset
+        k_nearest_neighbors=np.argsort(distances)[:self.k] #indexes of the k most similar examples in crescent order, where the first K will be the closest
 
-        match_class_mean = np.mean(k_similar_values)
-        
-        return match_class_mean
+        k_nearest_neighbors_values_labels=self._train_dataset.y[k_nearest_neighbors] #  y values of the shortest distance
+        return np.mean(k_nearest_neighbors_values_labels) #mesma logica mas agora aplico as medias
+
     
-    def predict(self, dataset:Dataset)->np.ndarray:
+    def predict(self,dataset:Dataset) -> np.ndarray:
         """
-        It predicts the classes of the given dataset
+        It predicts the mean label values of the given dataset
+        Go to every sample and calculate the distance of each sample(line dataset) to the rest of the dataset
 
         Parameters
         ----------
@@ -75,15 +93,13 @@ class KNNRegressor:
         Returns
         -------
         predictions: np.ndarray
-            The predictions of the model
+            The predictions of the model, with the supose values
         """
-        return np.apply_along_axis(self._get_closest_label, axis = 1, arr = dataset.X)
-        #applying the function to all lines of the dataset (not specific to test or train)
-        #checks distances between samples, selects k nearest and does the mean of y values of those
-
-    def score(self, dataset: Dataset) -> np.ndarray:
+        return np.apply_along_axis(self._get_closest_value_label,axis=1,arr=dataset.X) #
+       
+    def score(self,dataset:Dataset) -> float:
         """
-        It returns the accuracy of the model on the given dataset
+        It returns the rmse of the model on the given dataset
 
         Parameters
         ----------
@@ -92,10 +108,34 @@ class KNNRegressor:
 
         Returns
         -------
-        accuracy: float
-            The accuracy of the model
-        """
-        predictions = self.predict(dataset)
-        return rmse(dataset.y, predictions)
-    
+        rmse: float
+            The rmse(error) of the model
+        """       
+        predictions=self.predict(dataset)
+        return rmse(dataset.y,predictions) #compares the predicted with real values
 
+
+if __name__ == '__main__':
+    num_samples = 600
+    num_features = 100
+
+    X = np.random.rand(num_samples, num_features)
+    y = np.random.rand(num_samples)  # Valores apropriados para regressão
+
+    dataset_ = Dataset(X=X, y=y)
+
+    #features and class name 
+    dataset_.features = ["feature_" + str(i) for i in range(num_features)]
+    dataset_.label = "target"
+
+    dataset_train, dataset_test = train_test_split(dataset_, test_size=0.2)
+
+    # regressor KNN
+    knn_regressor = KNNRegressor(k=5)  
+
+    # fit the model to the train dataset
+    knn_regressor.fit(dataset_train)
+
+    # evaluate the model on the test dataset
+    score = knn_regressor.score(dataset_test)
+    print(f'The rmse of the model is: {score}')
